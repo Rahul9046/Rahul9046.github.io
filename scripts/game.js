02c9ea04a628db3c64632c79014dbadd7d385fba
Rahul Das <rahuld707@gmail.com>
2017-07-19 13:13:41 +0530

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,8 @@
 
-var paper=new R_svg("container",1270,650),buttons=[],sequence=[],checking=[],game_tout,count=1,strict_mode=false,
+
+
+//creation of UI elements and variables which will be used throughout the program.
+var paper=new R_svg("container",1270,650),buttons=[],sequence=[],checking=[],game_tout,count=1,
     pallete1=["darkred","forestgreen","goldenrod","indigo"],
     pallete2=["#ff3333","springgreen","#ffff66","mediumslateblue"];
     bg_image=paper.img(-297,0,1270,650,"images/Background.png").attr({"scale":[1.9,1]});
@@ -29,9 +32,10 @@ var paper=new R_svg("container",1270,650),buttons=[],sequence=[],checking=[],gam
     start_text=paper.text(600,387,"START").attr({"text-anchor":"middle","font-family":"Zamyatin","font-size":15}),
     strict_text=paper.text(660,387,"STRICT").attr({"text-anchor":"middle","font-family":"Zamyatin","font-size":15}); 
 
-var start_again=(function(strict_mode){
+
+var start_again=(function(){ //called when the user does any error or when the timeout finishes
     buttons.forEach(function(element){
-                    element.dataset.clickable=false; 
+                    element.dataset.clickable=false; //user cannot click the buttons when the sequence is flashing
                     });
     clearTimeout(game_tout);
     count--;
@@ -48,7 +52,7 @@ var start_again=(function(strict_mode){
     setTimeout(function(){
     count_value.attr({"fill":["red"]});
     },1000);
-    if( strict_btn.dataset.mode=="1"){
+    if( strict_btn.dataset.mode=="1"){//when in strict mode,reset the initial sequence and the timeout 
         setTimeout(function(){
         stopGame();
         strict_indicator.attr({"fill":["red"]});
@@ -73,59 +77,59 @@ var start_again=(function(strict_mode){
     }
     else{
         setTimeout(function(){
-        startGame();
+        startGame();//in non-strict mode,carry on the game with the recent sequence
         },1200);
     }
     });
     
 for(var i=0;i<4;i++){
     (function(j){
-        buttons[j].setAttribute("data-clickable",false);
-        buttons[j].setAttribute("data-number",i);
-        buttons[j].click(function(){
-        if(this.dataset.clickable==="true"){
-            if(this.dataset.number==checking.shift()){
+        buttons[j].setAttribute("data-clickable",false);//setting each button's clickable attribute to false initially 
+        buttons[j].setAttribute("data-number",i);//setting each button's identifier(0-4)
+        buttons[j].click(function(){   //assining click handler on each button.  
+        if(this.dataset.clickable==="true"){ //handler gets executed only if its clickable property is true.
+            if(this.dataset.number==checking.shift()){//matching the button clicked with the sequence
             this.attr({"fill":[pallete2[j]]});
              setTimeout(function(){
             buttons[j].attr({"fill":[pallete1[j]]});
             },300);
             clearTimeout(game_tout);
-            if(checking.length!==0){
+            if(checking.length!==0){//sequence is still left.
                 game_tout=setTimeout(function(){
-                start_again(strict_mode);  
+                start_again();  
                 },5000);
             }
-            else{
-                if(count_value.innerHTML==="20"){
+            else{ // sequence completed
+                if(count_value.innerHTML==="20"){//user wins when completed level 20.
                    stopGame();
                    setTimeout(function(){
-                   alert("Congo!!! You Have Won....Press Ok to start again");
+                   alert("Congo!!! You Have Won....Press Start to play again");
                    },600);
                 }
                 else{
                    buttons.forEach(function(element){
-                    element.dataset.clickable=false; 
-                    });
-                    startGame();
+                    element.dataset.clickable=false; //setting the clickable attribute of the buttons to false during the next sequence.
+                    }); 
+                    startGame();//start new level of n buttons with the first (n-1) buttons similar to last sequence.
                 }
             } 
-        }  
+            }  
             else{
-                start_again(strict_mode);
-                }
+                start_again(); //shows error and starts again in the same level(non-strict) or 1st level(strict).
+            }
         }        
         });   
     })(i);
 }
 
-var startGame=(function(){
-                  var r_btn_num;
+var startGame=(function(){ // gets executed for every new sequence
+                  var r_btn_num;// stores the random button number generated 
                   for(var i=0;i<count;i++){
-                  if(!sequence[i]){
+                  if(!sequence[i]){//empty value at that index
                        r_btn_num=Math.ceil(Math.random()*3);
                        sequence.push(r_btn_num);
                   }
-                  else{
+                  else{ // contains value from the previous sequence
                       r_btn_num=sequence[i];
                   }
                   (function(j,k){                    
@@ -134,15 +138,15 @@ var startGame=(function(){
                       count_value.innerHTML=count>9?count:"0"+count;
                       if(k===(count-1)){
                         var index=0;
-                        buttons.forEach(function(element){
+                        buttons.forEach(function(element){//setting clickable attribute of the button to true after the flashes.
                         element.dataset.clickable=true; 
                         });
                         count++;
                         sequence.forEach(function(elem){
                         checking[index++]=elem;
                         });
-                        game_tout=setTimeout(function(){
-                        start_again(strict_mode);    
+                        game_tout=setTimeout(function(){ //creating timeout for a sequence.gets reseted after each correct click.
+                        start_again();    
                         },5000);
                        }
                        buttons[j].attr({"fill":[pallete2[j]]});
@@ -150,11 +154,12 @@ var startGame=(function(){
                        buttons[j].attr({"fill":[pallete1[j]]});
                        },600);
                     }
-                    },1400+1500*k);   
+                    },1400+1500*k);//1200ms for the count display flash,200ms wait time after the buttons start flashing,   
+                                   //1500*k is the wait time for button in the sequence after which it flashes for 600ms(k=0 to 3).
                   })(r_btn_num,i);
                  }
              });
-var stopGame=(function(){
+var stopGame=(function(){  //called when the game finishes or the user stops the game from the stop button
      count_value.innerHTML="- -";
      count_value.attr({"fill":["brown"]}); 
      sequence=[];
@@ -168,10 +173,10 @@ var stopGame=(function(){
     strict_btn.dataset.mode=0; 
     });
 
-switch_btn.setAttribute("data-mode",0);
-strict_btn.setAttribute("data-mode",0);
+switch_btn.setAttribute("data-mode",0);// modes for switch button:0-off,1-on
+strict_btn.setAttribute("data-mode",0);// modes for strict button:0-non-strict,1-strict
 
-switch_btn.click(function(){
+switch_btn.click(function(){ //toggles ON/OFF button
     if(switch_btn.dataset.mode==="0"){
         switch_btn.setAttribute("x",Number(switch_btn.getAttribute("x"))+20);
         switch_btn.dataset.mode=1;
@@ -184,7 +189,7 @@ switch_btn.click(function(){
     }    
 });
 
-switch_bg.click(function(){
+switch_bg.click(function(){ //toggles ON/OFF button
     if(switch_btn.dataset.mode==="0"){
         switch_btn.setAttribute("x",Number(switch_btn.getAttribute("x"))+20);
         switch_btn.dataset.mode=1;
@@ -197,13 +202,13 @@ switch_bg.click(function(){
     }    
 });
 
-start_btn.click(function(){
+start_btn.click(function(){ //executed when the user clicks the start button and the game starts. 
     start_btn.setAttribute("r",12);
     setTimeout(function(){
     start_btn.setAttribute("r",13);
     },100);
     
-    if(switch_btn.dataset.mode==1){
+    if(switch_btn.dataset.mode==1){ //execute only if the button start button is set to ON.
         count_value.attr({"fill":["red"]});
         setTimeout(function(){
         count_value.attr({"fill":["brown"]});
@@ -223,7 +228,7 @@ start_btn.click(function(){
     }
 });
 
-strict_btn.click(function(){
+strict_btn.click(function(){ //toggles the strict mode.
     strict_btn.setAttribute("r",12);
     setTimeout(function(){
     strict_btn.setAttribute("r",13);
